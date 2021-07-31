f996cf62fcf3d0bdc717e34bed1980c13571e4e2
yangjunhyuk333 <chuchoun1234@gmail.com>
2021-07-31 19:15:14 +0900

    [ADD] 기타

--- README.md
+++
@@ -1,2 +0,0 @@
-"# simpleMemoJunhyuk" 
-"# simpleMemoJunhyuk" 
--- app/src/androidTest/java/com/junhyuk/simplememojunhyuk/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/junhyuk/simplememojunhyuk/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.junhyuk.simplememojunhyuk", appContext.packageName)
+        assertEquals("com.junhyuk.myapplication", appContext.packageName)
     }
 }
